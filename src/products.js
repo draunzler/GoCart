63b34e0ebd85ada6ae517a59c2924bd383c4cb28
Draunzler <shadymeister2017@gmail.com>
2023-07-01 00:51:53 +0530

    commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.webp';
+import product7 from './assets/7.webp';
+import product8 from './assets/8.webp';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productNAme: "iPhone",
+        price: 999.00,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productNAme: "AMcbook PRo 2022 (M1)",
+        price: 1999.00,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productNAme: "Cannon M50 Camera",
+        price: 699.00,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productNAme: "WLS Van Gogh Denim Jacket",
+        price: 228.00,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productNAme: "LED Light Strips",
+        price: 19.99,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productNAme: "SPECTRUM LS TEE",
+        price: 68.00,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productNAme: "AUTO SERVICE SHIRT by GOLF WANG",
+        price: 120.00,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productNAme: "DON'T TRIP UNSTRUCTURED HAT",
+        price: 40.00,
+        productImage: product8,
+    },
+];
