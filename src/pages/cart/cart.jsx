b068b9294e9b19cb5848716545ef23083dc4be9b
Draunzler <shadymeister2017@gmail.com>
2023-07-01 18:03:12 +0530

    commit

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { PRODUCTS } from '../../products';
 import { ShopContext } from '../../context/shop-context';
 import { CartItem } from './cart-item';
+import './cart.css';
 
 export const Cart = () => {
   const { cartItems } = useContext(ShopContext);
@@ -14,7 +15,7 @@ export const Cart = () => {
       <div className='cartItems'>
         {PRODUCTS.map((product) => {
           if(cartItems[product.id] > 0){
-            return <CartItem data={products} />;
+            return <CartItem data={product} />;
           }
         })}
       </div>
