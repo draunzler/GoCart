05a602be2f59ab062bc4fa13d26b1637541cea69
Draunzler <shadymeister2017@gmail.com>
2023-07-01 14:18:45 +0530

    commit

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext } from '../../context/shop-context';
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { addToCart } = useContext(ShopContext);
   return (
     <div className='product'>
         <img src={productImage} alt={productName}/>
@@ -11,7 +13,7 @@ export const Product = (props) => {
             </p>
             <p> ${price} </p>
         </div>
-        <button className="addToCartBttn"> Add To Cart</button>
+        <button className="addToCartBttn" onClick={() => addToCart(id)}> Add To Cart</button>
     </div>
   )
 };
